26c28de2210ff233d76b8ed54037aa456a254024
WebDevJL <webdev.jl@gmail.com>
2015-11-13 17:09:25 +0100

    adding Angular and font awesome

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 //http://vanilla-js.com/
 var content = document.getElementById("content");
+var CalendarApp = angular.module('CalendarApp', []);
+CalendarApp.factory('CalendarApp', function () {
+	var data = {};
+});
+
+CalendarApp.controller('CalendarController', function($scope) {
+
+});
+CalendarApp.controller('DayTypeController', function($scope) {
+
+});
+CalendarApp.controller('SettingsController', function($scope) {
+
+});
+CalendarApp.controller('SaveMyDataController', function($scope) {
+
+});
 
 function init() {
 	localforage.setDriver([localforage.WEBSQL, localforage.INDEXEDDB, localforage.LOCALSTORAGE]);
 }
+
 function save(key, data) {
 	return localforage.setItem(key, JSON.stringify(data));
 }
-function retrieve(key) {
+
+function runApp(key) {
 	return localforage.getItem(key);
 }
 
@@ -15,6 +34,7 @@ init();
 //load the basic app structure in the storage
 save("calendarApp", calendarApp);
 //retrieve the app 
-retrieve("calendarApp").then(function(data) {
-	content.innerHTML = data;
+runApp("calendarApp").then(function(data) {
+	CalendarApp.data = JSON.parse(data);
+ 	console.log(CalendarApp.data);
 });
